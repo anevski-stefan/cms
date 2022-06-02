bab1e36e50b6dba9c362bbf9f9e269626aec930d
anevski-stefan <anevskistefan84@gmail.com>
2022-06-02 18:19:18 +0200

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,10 +1,16 @@
 <?php
 
+    //DEVELOPMENT CONNECTION
+    // $db['db_host'] = "localhost";
+    // $db['db_user'] = "root";
+    // $db['db_pass'] = "";
+    // $db['db_name'] = "cms";
 
-    $db['db_host'] = "localhost";
-    $db['db_user'] = "root";
-    $db['db_pass'] = "";
-    $db['db_name'] = "cms";
+    //REMOTE DATABASE CONNECTION
+    $db['db_host'] = "sql11.freemysqlhosting.net";
+    $db['db_user'] = "sql11497285";
+    $db['db_pass'] = "YkKiVqkeDx";
+    $db['db_name'] = "sql11497285";
 
     foreach($db as $key => $value){
         define(strtoupper($key), $value);
