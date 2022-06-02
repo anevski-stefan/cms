85e5dae197e6afa180d4359f3719ad36f21cba40
anevski-stefan <anevskistefan84@gmail.com>
2022-06-02 17:34:21 +0200

    Initial commit

---
+++ admin/includes/add_user.php
@@ -0,0 +1,58 @@
+<?php
+    if(isset($_POST['create_user'])){
+        $username = escape($_POST['username']);
+        $user_password = escape($_POST['user_password']);
+        $user_firstname = escape($_POST['user_firstname']);
+        $user_lastname = escape($_POST['user_lastname']);
+        $user_email = escape($_POST['user_email']);
+        // $user_image = $_POST['user_image'];
+        $user_role = escape($_POST['user_role']);
+
+        // move_uploaded_file($post_image_tmp, "../images/$post_image");
+        $query = "INSERT INTO users(user_firstname, user_lastname, user_role, username, user_email, user_password) ";
+        $query .= "VALUES('{$user_firstname}', '{$user_lastname}', '{$user_role}', '{$username}', '{$user_email}', '{$user_password}' ) ";
+    
+        $create_user_query = mysqli_query($connection, $query);
+        confirmQuery($create_user_query);
+
+        echo "User Created: " . " " . "<a href='users.php'>View Users</a>";
+    }
+?>
+
+<form action="" method="post" enctype="multipart/form-data">
+    <div class="form-group">
+        <label for="title">First Name</label>
+        <input type="text" class="form-control" name="user_firstname">
+    </div>
+    <div class="form-group">
+        <label for="post_status">Last Name</label>
+        <input type="text" class="form-control" name="user_lastname">
+    </div>
+    <div class="form-group">
+        <select name="user_role" id="user_role">
+            <option value="subscriber">Select Options</option>
+            <option value="admin">Admin</option>
+            <option value="subscriber">Subscriber</option>
+        </select>
+    </div>
+    <!-- <div class="form-group">
+        <label for="post_image">Post Image</label>
+        <input type="file" name="image">
+    </div> -->
+    
+    <div class="form-group">
+        <label for="post_tags">Username</label>
+        <input type="text" class="form-control" name="username">
+    </div>
+    <div class="form-group">
+        <label for="post_content">Email</label>
+        <input type="email" class="form-control" name="user_email">
+    </div>
+    <div class="form-group">
+        <label for="post_content">Password</label>
+        <input type="password" class="form-control" name="user_password">
+    </div>
+    <div class="form-group">
+        <input type="submit" class="btn btn-primary" name="create_user" value="Add User">
+    </div>
+</form>
